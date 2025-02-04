cb1254382dfe4aa552ea2eaa44b23e4b87e21a7d
Anna Karlova <avkarlova90@yandex.ru>
2025-02-04 22:14:43 +0300

    неделя5 seeds

--- models/intermediate/flights/fct_tickets.sql
+++ models/intermediate/flights/fct_tickets.sql
@@ -11,3 +11,9 @@ select
     contact_data
 from
     {{ ref('stg_flights__facts__tickets') }}
+    WHERE
+        passenger_id not in
+        (
+            select passenger_id
+            from {{ ref('passenger_id') }}
+        )
