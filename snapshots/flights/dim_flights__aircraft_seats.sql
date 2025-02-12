aee92e4967bbc50fc115e59184d57ce488f7a835
Anna Karlova <avkarlova90@yandex.ru>
2025-02-12 20:57:25 +0300

    исправила

--- snapshots/flights/dim_flights__aircraft_seats.sql
+++ snapshots/flights/dim_flights__aircraft_seats.sql
@@ -3,10 +3,10 @@
 {{
    config(
        target_schema='snapshot',
-       unique_key='aircraft_code',
+       unique_key=['aircraft_code', 'seat_no'],
 
        strategy='check',
-       check_cols = ['seat_no', 'fare_conditions'],
+       check_cols = ['fare_conditions'],
        dbt_valid_to_current="'9999-01-01'::date"
    )
 }}
